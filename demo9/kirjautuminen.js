36c91c8b52b12475f09494edc2270073da4c0c15
RoopeHolopainen <roope.holopainen@edu.keuda.fi>
2022-03-07 10:51:50 +0200

    ggggggg

--- demo9/kirjautuminen.js
+++ demo9/kirjautuminen.js
@@ -5,9 +5,17 @@ function onkoKirjautunut(){
         document.getElementById("tervetuloa_teksti").textContent += ` ${localStorage.getItem("nimi")}`;
         document.getElementById("kirjautumis_lomake").style.display = "none";
         document.getElementById("kirjauduUlos").style.display = "block";
+        document.getElementById("tervetuloa_teksti").style.display = "block";
     }
 }
 function kirjaudu(){
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kyllä");
+}
+function kirjauduUlos(){
+    localStorage.clear();
+    document.getElementById("kirjautumis_lomake").style.display = "block";
+    document.getElementById("kirjauduUlos").style.display = "none";
+    document.getElementById("tervetuloa_teksti").style.display = "none";
+    
 }
