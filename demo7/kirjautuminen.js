1512b21010aceca69d569ca25c27a449fcbfd15a
RoopeHolopainen <roope.holopainen@edu.keuda.fi>
2022-03-07 09:04:51 +0200

    demo7  hauseee

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
     let kirjautunut = 'kylla';
     if(kirjautunut === 'kylla'){
-        document.getElementById("tervetuloa_teksti").textContent +='Roope!';
+        document.getElementById("tervetuloa_teksti").textContent +=' Roope!';
     }
 }
