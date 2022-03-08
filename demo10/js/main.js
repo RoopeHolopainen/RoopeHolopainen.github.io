88c3f58b88571d506b6510954d816ca0fe1f4e96
RoopeHolopainen <roope.holopainen@edu.keuda.fi>
2022-03-08 08:53:13 +0200

    aaaaaaaaaaaaaa

--- demo10/js/main.js
+++ demo10/js/main.js
@@ -32,4 +32,26 @@
         return false;
     });
 })(jQuery);
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut(){
+    if (localStorage.getItem("kirjautunut") === "kyllä"){
+        document.getElementById("tervetuloa_teksti").textContent += ` ${localStorage.getItem("nimi")}`;
+        document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("kirjauduUlos").style.display = "block";
+        document.getElementById("tervetuloa_teksti").style.display = "block";
+    }
+}
+function kirjaudu(){
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+    localStorage.setItem("kirjautunut", "kyllä");
+}
+function kirjauduUlos(){
+    localStorage.clear();
+    //mddddddddddd
+    document.getElementById("kirjautumis_lomake").style.display = "block";
+    document.getElementById("kirjauduUlos").style.display = "none";
+    document.getElementById("tervetuloa_teksti").style.display = "none";
+    
+}
 
